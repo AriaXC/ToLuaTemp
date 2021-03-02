c535c7ea93a4be2b17f820b5e6bb646541349ca1
Aria <1243372862@qq.com>
2021-03-02 18:15:06 +0800

    六边形

---
+++ Assets/Test/AMap/CreateMap.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateMap : MonoBehaviour
+{
+    public int wight = 10;
+    public int height = 10;
+    public GameObject child;
+
+    GameObject[] hexs;
+    //外半径
+    public const float outerRadius = 10f;
+    //内半径
+    public const float innerRadius = outerRadius * 0.866025404f;
+    public static Vector3[] centers = {
+        new Vector3(-outerRadius,0,0),
+        new Vector3(-outerRadius*0.5f,0,innerRadius),
+        new Vector3(outerRadius*0.5f,0,innerRadius),
+        new Vector3(outerRadius,0,0),
+        new Vector3(outerRadius*0.5f,0,-innerRadius),
+        new Vector3(-outerRadius*0.5f,0,-innerRadius),
+    };
+
+    //public Canvas gridCanvas;
+    public HexMesh gridMesh;
+    private void Awake()
+    {
+        hexs = new GameObject[wight*height];
+        int index = 0;
+        for (int i = 0; i < wight; ++i)
+        {
+            for (int j = 0; j < height; ++j)
+            {
+                AddMap(i, j, index);
+                index++;
+            }
+        }
+
+        //gridCanvas = GetComponentInChildren<Canvas>();
+        gridMesh = GetComponentInChildren<HexMesh>();
+    }
+    private void Start()
+    {
+        //对网格进行拆分
+       gridMesh.Triangulate(hexs);
+    }
+    private void AddMap(int i,int j,int index)
+    {
+        Vector3 pos;
+        pos.x = (i+ j*0.5f - j/2) * innerRadius*2;
+        pos.y = 0;
+        pos.z = j * outerRadius*1.5f;
+
+        hexs[index] = Instantiate(child, this.transform);
+        hexs[index].transform.localPosition = pos;
+        hexs[index].transform.Find("Text").gameObject.GetComponent<TextMesh>().text = index.ToString();
+    }
+}
+
+
---
+++ Assets/Test/AMap/HexMesh.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMesh : MonoBehaviour
+{
+    Mesh hexMesh;
+    List<Vector3> vertList;
+    List<int> triangles;
+
+
+    private void Awake()
+    {
+        hexMesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = hexMesh ;
+
+        gameObject.GetComponent<MeshRenderer>().material =GameObject.Find("Plane").GetComponent<MeshRenderer>().material;
+
+
+        hexMesh.name = "HexMash";
+        vertList = new List<Vector3>();
+        triangles = new List<int>();
+
+    }
+
+
+    public void Triangulate(GameObject[] hexs)
+    {
+        vertList.Clear();
+        triangles.Clear();
+        hexMesh.Clear();
+        for (int i = 0; i < hexs.Length; i++)
+        {
+            Triangulate(hexs[i]);
+        }
+        hexMesh.vertices = vertList.ToArray();
+        hexMesh.triangles = triangles.ToArray();
+        hexMesh.RecalculateNormals();
+    }
+    public void Triangulate(GameObject hex)
+    {
+        Vector3 pos = hex.transform.localPosition;
+        AddTriangulate(pos, pos+CreateMap.centers[2], pos+CreateMap.centers[1]);
+    }
+
+    //根据三角形的三个顶点 绘制三角形
+    void AddTriangulate(Vector3 v1,Vector3 v2,Vector3 v3)
+    {
+        int index = vertList.Count;
+        vertList.Add(v1);
+        vertList.Add(v2);
+        vertList.Add(v3);
+
+        triangles.Add(index);
+        triangles.Add(index + 1);
+        triangles.Add(index + 2);
+    }
+}
