315ba4fe869fc537013aa628f6a6cec7ac008f6b
Aria <1243372862@qq.com>
2021-04-21 16:44:09 +0800

    日志写入 未

---
+++ Assets/LuaFramework/MoonScripts/Util/LogFileWriter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 日志写入 
+public static class LogFileWriter
+{
+
+    //public static readonly string _path =Application.dataPath + ""
+}
