be7ad9e5e2c280208643dafe194f267f01403516
Aria <1243372862@qq.com>
2021-04-23 11:23:18 +0800

    缓存池 未

--- Assets/Lua/Aria/Utils/PrefabPool.lua
+++ Assets/Lua/Aria/Utils/PrefabPool.lua
@@ -1,3 +1,21 @@
-
-
 ---对象池
+
+local  PrefabPool = {}
+
+
+function  PrefabPool.Get(prefabPath,parent)
+	
+end
+
+function PrefabPool.Recycle( go,prefabPath )
+	
+end
+
+function  PrefabPool.Clean(type)
+	if type then
+		--创建 缓存池的父节点
+	else
+	end
+end
+
+return  PrefabPool 
--- Assets/Lua/Module/MyGame/FootBall_1/FootBallView.lua
+++ Assets/Lua/Module/MyGame/FootBall_1/FootBallView.lua
@@ -3,16 +3,15 @@
 function  FootBallView:Ctor( )
 	-- body
 	FootBallView.super.Ctor(self,"Prefabs/MyGame/FootBall/FootView.prefab",GameConst.Layer.ui)
-
 end
 
 function  FootBallView:OnInitialize( ... )
-	-- body
 	FootBallView.super.OnInitialize(self)
+	
 	self:AddClick()
 end
 function  FootBallView:AddClick( ... )
-
+	
 end
 
 return FootBallView
